e92b8958ced320706e1fa7089057296514864c52
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-15 00:09:54 +0300

    style:add comments and blog modelles

--- app/models.py
+++ app/models.py
@@ -1,15 +1,16 @@
+from datetime import datetime
 from . import db
 from flask_login import  UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(UserMixin ,db.Model):
-    __tablename__='users'
+    __tablename__='user'
     id= db.Column(db.Integer, primary_key=True) 
     username = db.Column(db.String , unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     bio = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
-
+    blogs = db.relationship('Blogs', backref='author', lazy = 'dynamic')
     @property
     def password(self):
         raise AttributeError('You cannnot read the password attribute')
@@ -21,11 +22,43 @@ class User(UserMixin ,db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
+    
+class Blogs(db.Model):  
+    __tablename__='blog'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.String())
+    date_created=db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    comments = db.relationship('comment',backref='blog' ,lazy='dynamic' )
+    
+    def save_blogs(self):
+        db.session.add(self)
+        db.session.commit()
+    def repr(self):
+        return f'Post {self.title}'
+    
+    
 class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    date_posted= db.Column(db.DateTime,default=datetime.utcnow)
+    blog_id= db.Column(db.ForeignKey("blog.id")) 
+    user_id= db.Column(db.ForeignKey("user.id")) 
     
-    id = db.Column(db.models.Integer, primary_key=True)
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'Comment {self.name}'
     
-
+    
+    
+    
+    
+    
+    
 class Quotes:
     """
      '''
