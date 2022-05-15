261dec5bcecd82cdc362027566361d336eb40a5d
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-16 01:01:13 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:oderoh@localhost:5432/jkblog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://qteqfmkfxbajji:0992358cf0406df0354201f3de19624228a6519cf333469d67f309b811ce2813@ec2-3-229-11-55.compute-1.amazonaws.com:5432/d9rvmqin9pm26k'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     pass
     
@@ -30,8 +30,8 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:oderoh@localhost/jkblog'
     
-    SQLALCHEMY_DATABASE_URI = 'postgresql://qteqfmkfxbajji:0992358cf0406df0354201f3de19624228a6519cf333469d67f309b811ce2813@ec2-3-229-11-55.compute-1.amazonaws.com:5432/d9rvmqin9pm26k'
     DEBUG = True
     
 config_options = {
