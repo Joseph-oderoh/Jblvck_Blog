929b430c8db3b55b41471b7c76ce9a5883dbe3a5
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-05-15 23:55:48 +0300

    style:add production deve configs

--- config.py
+++ config.py
@@ -4,13 +4,34 @@ class Config:
    QUOTES_API_URL='http://quotes.stormconsultancy.co.uk/random.json'
    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:oderoh@localhost/jkblog'
    SECRET_KEY = os.environ.get('SECRET_KEY')
+   
+# email configurations
+   MAIL_SERVER = 'smtp.googlemail.com'
+   MAIL_PORT = 587
+   MAIL_USE_TLS = False
+   MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    
 class ProdConfig(Config):
-    pass 
-
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:oderoh@localhost:5432/jkblog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    pass
+    
+    
+    
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SUBJECT_PREFIX = 'upitch'
+    DEBUG = True
 
 
 class DevConfig(Config):
     
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:oderoh@localhost/jkblog'
     DEBUG = True
     
 config_options = {
--- manage.py
+++ manage.py
@@ -3,9 +3,11 @@ from app import create_app,db
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
+from app.models import Blog, User
+
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 
 manager = Manager(app)
@@ -21,6 +23,6 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db )
+    return dict(app = app,db = db,User=User,Blog = Blog )
 if __name__ == '__main__':
     manager.run()
